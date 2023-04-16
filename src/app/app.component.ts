30d5f5f361e4261ba93454a80c722a2af47f35a3
Banz <banz+popmanga@vivaldi.net>
2023-04-16 04:59:53 +0200

    POC

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { data } from "./data";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+  levels = data;
+}
---
+++ src/app/data.ts
@@ -0,0 +1,593 @@
+export interface Entry {
+  title: string;
+  animeTitle?: string;
+  imageUrl: string;
+}
+
+export interface Level {
+  level: number;
+  entries: Entry[];
+}
+
+export const data: Level[] = [
+  {
+    level: 10,
+    entries: [
+      {
+        title: "Pokémon",
+        imageUrl: "pokemon.jpg",
+      },
+      {
+        title: "Hello Kitty",
+        imageUrl: "hello-kitty.jpg",
+      },
+      {
+        title: "The Legend of Zelda",
+        imageUrl: "zelda.jpg",
+      },
+      {
+        title: "One Piece",
+        imageUrl: "one-piece.jpg",
+      },
+      {
+        title: "Dragon Ball",
+        imageUrl: "",
+      },
+      {
+        title: "Mario",
+        imageUrl: "",
+      },
+      {
+        title: "Naruto",
+        imageUrl: "",
+      },
+      {
+        title: "Attaque des Titans",
+        imageUrl: "",
+      },
+      {
+        title: "Saint Seiya",
+        animeTitle: "Chevaliers du Zodiaque",
+        imageUrl: "",
+      },
+      {
+        title: "Yu-Gi-Oh",
+        imageUrl: "",
+      },
+      {
+        title: "Albator",
+        imageUrl: "",
+      },
+      {
+        title: "Goldorak",
+        imageUrl: "",
+      },
+      {
+        title: "Death Note",
+        imageUrl: "",
+      },
+      {
+        title: "Beyblade",
+        imageUrl: "",
+      },
+      {
+        title: "Digimon",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 9,
+    entries: [
+      {
+        title: "Fairy Tail",
+        imageUrl: "",
+      },
+      {
+        title: "Full alchemist",
+        imageUrl: "",
+      },
+      {
+        title: "Detective Conan",
+        imageUrl: "",
+      },
+      {
+        title: "Captain Tsubasa",
+        animeTitle: "Olive et Tom",
+        imageUrl: "",
+      },
+      {
+        title: "My Hero Academia",
+        imageUrl: "",
+      },
+      {
+        title: "JoJo",
+        imageUrl: "",
+      },
+      {
+        title: "City Hunter",
+        animeTitle: "Nicky Larson",
+        imageUrl: "",
+      },
+      {
+        title: "Hunter x Hunter",
+        imageUrl: "",
+      },
+      {
+        title: "Bleach",
+        imageUrl: "",
+      },
+      {
+        title: "Demon Slayer",
+        imageUrl: "",
+      },
+      {
+        title: "Kirby",
+        imageUrl: "",
+      },
+      {
+        title: "Evangelion",
+        imageUrl: "",
+      },
+      {
+        title: "One Punch Man",
+        imageUrl: "",
+      },
+      {
+        title: "Sword Art Online",
+        imageUrl: "",
+      },
+      {
+        title: "Sailor Moon",
+        imageUrl: "",
+      },
+      {
+        title: "Tokyo Ghoul",
+        imageUrl: "",
+      },
+      {
+        title: "Seven Deadly Sins",
+        imageUrl: "",
+      },
+      {
+        title: "Food wars",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 8,
+    entries: [
+      {
+        title: "Berserk",
+        imageUrl: "",
+      },
+      {
+        title: "Akira",
+        imageUrl: "",
+      },
+      {
+        title: "Assassination Classroom",
+        imageUrl: "",
+      },
+      {
+        title: "Kurokos Basket",
+        imageUrl: "",
+      },
+      {
+        title: "Card Captor Sakura",
+        imageUrl: "",
+      },
+      {
+        title: "Hokuto No Ken",
+        animeTitle: "Ken le survivant",
+        imageUrl: "",
+      },
+      {
+        title: "Vinland Saga",
+        imageUrl: "",
+      },
+      {
+        title: "Black Clover",
+        imageUrl: "",
+      },
+      {
+        title: "Dr Stone",
+        imageUrl: "",
+      },
+      {
+        title: "Promised Neverland",
+        imageUrl: "",
+      },
+      {
+        title: "Hamtaro",
+        imageUrl: "",
+      },
+      {
+        title: "La rose de Versailles",
+        animeTitle: "Lady Oscar",
+        imageUrl: "",
+      },
+      {
+        title: "Slam Dunk",
+        imageUrl: "",
+      },
+      {
+        title: "Nana",
+        imageUrl: "",
+      },
+      {
+        title: "Mega Man",
+        imageUrl: "",
+      },
+      {
+        title: "Cowboy Bebop",
+        imageUrl: "",
+      },
+      {
+        title: "Gundam",
+        imageUrl: "",
+      },
+      {
+        title: "Ippo",
+        imageUrl: "",
+      },
+      {
+        title: "Great Teacher Onizuka",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 7,
+    entries: [
+      {
+        title: "Eyeshield 21",
+        imageUrl: "",
+      },
+      {
+        title: "Ghost in the shell",
+        imageUrl: "",
+      },
+      {
+        title: "Gunnm",
+        imageUrl: "",
+      },
+      {
+        title: "Prince of tennis",
+        imageUrl: "",
+      },
+      {
+        title: "Cat's eyes",
+        imageUrl: "",
+      },
+      {
+        title: "Doraemon",
+        imageUrl: "",
+      },
+      {
+        title: "Fire force",
+        imageUrl: "",
+      },
+      {
+        title: "Gintama",
+        imageUrl: "",
+      },
+      {
+        title: "Alice in Borderland",
+        imageUrl: "",
+      },
+      {
+        title: "Monster",
+        imageUrl: "",
+      },
+      {
+        title: "Kenshin le vagabond",
+        imageUrl: "",
+      },
+      {
+        title: "Blue exorcist",
+        imageUrl: "",
+      },
+      {
+        title: "Haikyu",
+        imageUrl: "",
+      },
+      {
+        title: "Astro Boy",
+        imageUrl: "",
+      },
+      {
+        title: "Jeanne et Serge",
+        imageUrl: "",
+      },
+      {
+        title: "Battle Royale",
+        imageUrl: "",
+      },
+      {
+        title: "Fruit Basket",
+        imageUrl: "",
+      },
+      {
+        title: "Love Hina",
+        imageUrl: "",
+      },
+      {
+        title: "Kingdom Hearts",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 6,
+    entries: [
+      {
+        title: "Initial D",
+        imageUrl: "",
+      },
+      {
+        title: "Gantz",
+        imageUrl: "",
+      },
+      {
+        title: "Ranma 1/2",
+        imageUrl: "",
+      },
+      {
+        title: "Devilman",
+        imageUrl: "",
+      },
+      {
+        title: "Shin-Shan",
+        imageUrl: "",
+      },
+      {
+        title: "Kingdom",
+        imageUrl: "",
+      },
+      {
+        title: "Prison School",
+        imageUrl: "",
+      },
+      {
+        title: "Toriko",
+        imageUrl: "",
+      },
+      {
+        title: "Shi",
+        imageUrl: "",
+      },
+      {
+        title: "Soul eater",
+        imageUrl: "",
+      },
+      {
+        title: "Sun-Ken-Rock",
+        imageUrl: "",
+      },
+      {
+        title: "Georgie",
+        imageUrl: "",
+      },
+      {
+        title: "Perfect World",
+        imageUrl: "",
+      },
+      {
+        title: "Gambling school",
+        imageUrl: "",
+      },
+      {
+        title: "Lamu",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 5,
+    entries: [
+      {
+        title: "Shaman King",
+        imageUrl: "",
+      },
+      {
+        title: "Belzebuub",
+        imageUrl: "",
+      },
+      {
+        title: "D gray man",
+        imageUrl: "",
+      },
+      {
+        title: "Magi",
+        imageUrl: "",
+      },
+      {
+        title: "Black butler",
+        imageUrl: "",
+      },
+      {
+        title: "Maid sama",
+        imageUrl: "",
+      },
+      {
+        title: "Sawako",
+        imageUrl: "",
+      },
+      {
+        title: "Vagabond",
+        imageUrl: "",
+      },
+      {
+        title: "Dr Slump",
+        imageUrl: "",
+      },
+      {
+        title: "Fate",
+        imageUrl: "",
+      },
+      {
+        title: "Full metal panic fumoffu",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 4,
+    entries: [
+      {
+        title: "Zetman",
+        imageUrl: "",
+      },
+      {
+        title: "Tsubasa reservoir chronicles",
+        imageUrl: "",
+      },
+      {
+        title: "Danmachi",
+        imageUrl: "",
+      },
+      {
+        title: "Valkyrie apocalypse",
+        imageUrl: "",
+      },
+      {
+        title: "The breaker",
+        imageUrl: "",
+      },
+      {
+        title: "Bonne nuit punpun",
+        imageUrl: "",
+      },
+      {
+        title: "Rainbow",
+        imageUrl: "",
+      },
+      {
+        title: "Dorohedoro",
+        imageUrl: "",
+      },
+      {
+        title: "Kamakura diary",
+        imageUrl: "",
+      },
+      {
+        title: "Chihayafuru",
+        imageUrl: "",
+      },
+      {
+        title: "Satan 666",
+        imageUrl: "",
+      },
+      {
+        title: "Samurai champloo",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 3,
+    entries: [
+      {
+        title: "Inspecteur kurokichi",
+        imageUrl: "",
+      },
+      {
+        title: "Hana yori dango",
+        imageUrl: "",
+      },
+      {
+        title: "Blue spring ride",
+        imageUrl: "",
+      },
+      {
+        title: "Say I love you",
+        imageUrl: "",
+      },
+      {
+        title: "Kiss him not me",
+        imageUrl: "",
+      },
+      {
+        title: "Lovely complex",
+        imageUrl: "",
+      },
+      {
+        title: "Kimi wa pet",
+        imageUrl: "",
+      },
+      {
+        title: "Midnight secretary",
+        imageUrl: "",
+      },
+      {
+        title: "Bobobobo bobobo",
+        imageUrl: "",
+      },
+      {
+        title: "Suikoden",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 2,
+    entries: [
+      {
+        title: "Hoshin l'investiture des dieux",
+        imageUrl: "",
+      },
+      {
+        title: "Princess jellyfish",
+        imageUrl: "",
+      },
+      {
+        title: "Homonculus",
+        imageUrl: "",
+      },
+      {
+        title: "Ascension",
+        imageUrl: "",
+      },
+      {
+        title: "Uran high school host club",
+        imageUrl: "",
+      },
+      {
+        title: "Mon histoire",
+        imageUrl: "",
+      },
+      {
+        title: "Wolf girl and black prince",
+        imageUrl: "",
+      },
+      {
+        title: "Les vacances de Jésus et Boudha",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 1,
+    entries: [
+      {
+        title: "Golgo 13",
+        imageUrl: "",
+      },
+      {
+        title: "Kids on the slope",
+        imageUrl: "",
+      },
+    ],
+  },
+  {
+    level: 0,
+    entries: [
+      {
+        title: "Cicatrice the sirius",
+        imageUrl: "",
+      },
+    ],
+  },
+];
